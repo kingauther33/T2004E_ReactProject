d392ee1002837cc2ce3f66d758c7747dca7ef267
phong100401 <65002403+phong100401@users.noreply.github.com>
2021-08-26 18:23:01 +0700

    Revert "up footer"
    footer

--- reactproject/src/components/user/Footer.js
+++ reactproject/src/components/user/Footer.js
@@ -1,61 +1,73 @@
 import React from 'react';
-import Footer from '../pfooter/footer';
-import Icon from '../pfooter/icons';
 import styles from './Footer.module.css';
-import images from '../../assets';
+import {Link} from 'react-router-dom';
 
-const FooterContainer = () => {
+const Footer = () => {
 	return (
-		<>
-			<div className={[styles['container']]}>
-				{/* <img src={images.contactUs.avatar} alt="Test" /> */}
-				<img src="/dist/img/user1-128x128.jpg" alt="Test" />
-				<Footer.Wrapper>
-					<Footer.Row>
-						<Footer.Column>
-							<Footer.Title>About Us</Footer.Title>
-							<Footer.Link href="#">Story</Footer.Link>
-							<Footer.Link href="#">Clients</Footer.Link>
-							<Footer.Link href="#">Testimonials</Footer.Link>
-						</Footer.Column>
-						<Footer.Column>
-							<Footer.Title>Services</Footer.Title>
-							<Footer.Link href="#">Marketing</Footer.Link>
-							<Footer.Link href="#">Consulting</Footer.Link>
-							<Footer.Link href="#">Development</Footer.Link>
-							<Footer.Link href="#">Design</Footer.Link>
-						</Footer.Column>
-						<Footer.Column>
-							<Footer.Title>Contact Us</Footer.Title>
-							<Footer.Link href="#">United States</Footer.Link>
-							<Footer.Link href="#">United Kingdom</Footer.Link>
-							<Footer.Link href="#">Australia</Footer.Link>
-							<Footer.Link href="#">Support</Footer.Link>
-						</Footer.Column>
-						<Footer.Column>
-							<Footer.Title>Social</Footer.Title>
-							<Footer.Link href="#">
-								<Icon className="fab fa-facebook-f" />
-								Facebook
-							</Footer.Link>
-							<Footer.Link href="#">
-								<Icon className="fab fa-instagram" />
-								Instagram
-							</Footer.Link>
-							<Footer.Link href="#">
-								<Icon className="fab fa-youtube" />
-								Youtube
-							</Footer.Link>
-							<Footer.Link href="#">
-								<Icon className="fab fa-twitter" />
-								Twitter
-							</Footer.Link>
-						</Footer.Column>
-					</Footer.Row>
-				</Footer.Wrapper>
+		<div className={styles['container']}>
+			<div className={styles['wrapper']}>
+				<div className={styles['row']}>
+					<div className={styles['column']}>
+						<div className={styles['title']}>AboutUs</div>
+						<div>
+							<Link to="#" className={styles['link']}>Story</Link>
+						</div>
+						<div>
+							<Link to="#" className={styles['link']}>Clients</Link>
+						</div>
+						<div>
+							<Link to="#" className={styles['link']}>Testimonials</Link>
+						</div>
+					</div>
+					<div className={styles['column']}>
+						<div className={styles['title']}>Services</div>
+						<div>
+							<Link to="#" className={styles['link']}>Marketing</Link>
+						</div>
+						<div>
+							<Link to="#" className={styles['link']}>Consulting</Link>
+						</div>
+						<div>
+							<Link to="#" className={styles['link']}>Development</Link>
+						</div>
+						<div>
+							<Link to="#" className={styles['link']}>Design</Link>
+						</div>
+					</div>
+					<div className={styles['column']}>
+						<div className={styles['title']}>Contact Us</div>
+						<div>
+							<Link to="#" className={styles['link']}>United States</Link>
+						</div>
+						<div>
+							<Link to="#" className={styles['link']}>United Kingdom</Link>
+						</div>
+						<div>
+							<Link to="#" className={styles['link']}>Australia</Link>
+						</div>
+						<div>
+							<Link to="#" className={styles['link']}>Support</Link>
+						</div>
+					</div>
+					<div className={styles['column']}>
+						<div className={styles['title']}>Social</div>
+						<div>
+							<Link to="#" className={styles['link']}><i className="fab fa-facebook"></i>Facebook</Link>
+						</div>
+						<div>
+							<Link to="#" className={styles['link']}><i className="fab fa-instagram"></i>Instagram</Link>
+						</div>
+						<div>
+							<Link to="#" className={styles['link']}><i className="fab fa-youtube"></i>Youtube</Link>
+						</div>
+						<div>
+							<Link to="#" className={styles['link']}><i className="fab fa-twitter"></i>Twitter</Link>
+						</div>
+					</div>
+				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
-export default FooterContainer;
+export default Footer;
